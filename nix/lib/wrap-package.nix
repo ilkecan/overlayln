1a72b0bd4ee281bdcbef3f94bae9098e3cc8df86
ilkecan <ilkecan@protonmail.com>
2022-10-03 23:03:01 +0000

    Update flake lock

--- nix/lib/wrap-package.nix
+++ nix/lib/wrap-package.nix
@@ -13,12 +13,14 @@ let
     getBin
     getName
     getValues
+    pipe
   ;
 
   inherit (nix-alacarte)
     attrs
     list
     optionalValue
+    pair
     wrapExecutable
   ;
 
@@ -32,12 +34,15 @@ let
       ...
     }@args:
     let
-      args' = removeAttrs args [
-        "exePath"
-      ] // {
-        name = exeName;
-        outPath = exePath;
-      };
+      args' = pipe args [
+        (attrs.remove [
+          "exePath"
+        ])
+        (attrs.merge' {
+          name = exeName;
+          outPath = exePath;
+        })
+      ];
 
       wrappedExe = wrapExecutable "${drv}/${exePath}" args';
       linkedUp = linkup {
@@ -50,12 +55,14 @@ let
         let
           drv = if pkgIsBinDrv then pkg else drv;
         in
-        drv // (removeAttrs linkedUp [
+        drv
+        // attrs.remove [
           "all"
           "out"
           "outputName"
           "outputs"
-        ]) // {
+        ] linkedUp
+        // {
           inherit (drv) passthru;
           meta =
             linkedUp.meta
@@ -63,32 +70,31 @@ let
             // { inherit (linkedUp.meta) position; }
           ;
           ${drv.outputName or null} = wrappedDrv;
-        }
-      ;
+        };
 
       commonAttrs =
-        (removeAttrs (if pkgIsBinDrv then wrappedDrv else pkg) [
-          "override"
-          "overrideDerivation"
-        ])
+        (attrs.remove
+          [ "override" "overrideDerivation" ]
+          (if pkgIsBinDrv then wrappedDrv else pkg))
         // list.toAttrs outputList
-        // (attrs.optional (drv ? outputs) { inherit all outputs; })
-      ;
+        // (attrs.optional (drv ? outputs) { inherit all outputs; });
       outputs = drv.outputs or [ ];
       all = getValues outputList;
-      outputList = list.forEach outputs (outputName: {
-        name = outputName;
-        value = commonAttrs // {
-          inherit (wrappedDrv.${outputName})
-            drvPath
-            outPath
-            outputName
-            passthru
-            type
-          ;
-          outputSpecified = true;
-        };
-      });
+      outputList = list.forEach outputs (outputName:
+        let
+          value = commonAttrs // {
+            inherit (wrappedDrv.${outputName})
+              drvPath
+              outPath
+              outputName
+              passthru
+              type
+            ;
+            outputSpecified = true;
+          };
+        in
+        pair outputName value
+      );
     in
     commonAttrs // {
       ${optionalValue (pkg ? override) "override"} =
